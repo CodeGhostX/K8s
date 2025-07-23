5d67ef4587a65e5d18c70e2fa046933a70b2bcd3
Abhishek Kumar 1 <abhishek.kumar1@abhishekkumar1-MacBook-Pro.local>
2025-07-23 13:42:56 +0530

    ...

--- projects/demo/start.sh
+++ projects/demo/start.sh
@@ -1,6 +1,8 @@
-minikube start --nodes 2 --driver=docker
 cd k8s
+kind create cluster --config cluster.yml
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+helm install nginx-ingress ingress-nginx/ingress-nginx --namespace ingress-nginx --create-namespace
+kind load docker-image client
+kind load docker-image server
 kubectl apply -f persistent-volume.yml -f persistent-volume-claim.yml -f client-deployment.yml -f server-deployment.yml -f postgres-deployment.yml -f client-service.yml -f server-service.yml -f postgres-service.yml -f ingress.yml
-minikube image load server
-minikube image load client
-minikube addons enable ingress
